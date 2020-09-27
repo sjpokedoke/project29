1cb50b7261d0c453dbfbb64cec809fafe91cb31c
sjpokedoke <70572845+sjpokedoke@users.noreply.github.com>
2020-09-27 14:55:09 +0800

    Add files via upload

---
+++ box.js
@@ -0,0 +1,21 @@
+class Box{
+  constructor(x, y, width, height) {
+      var options = {
+        isStatic: false,
+          'restitution':0.4,
+          'friction':0,
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos = this.body.position
+      push();
+      translate(pos.x, pos.y);
+      rectMode(CENTER)
+      rect(0,0,this.width,this.height)
+      pop();
+    }
+}
